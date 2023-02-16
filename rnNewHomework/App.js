e7bd3aba860af262130153ad886d5561f93119ae
Serhii Denysiuk <106811130+CrabSerg@users.noreply.github.com>
2023-02-16 20:17:55 +0200

    Initial commit

---
+++ rnNewHomework/App.js
@@ -0,0 +1,12 @@
+import LoginScreen from "./Screens/components/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/components/RegistrationScreen/RegistrationScreen";
+
+export default function App() {
+  return (
+    <>
+      <RegistrationScreen />
+      <LoginScreen />
+    </>
+  );
+}
+
